e8a7d8d6ffa40e96a7f5c742df593684a306e303
xxherooorehxx <carloramosmuras@gmail.com>
2018-03-15 22:59:04 +0100

    Comentado

--- src/com/emu/gameboy/display/Display.java
+++ src/com/emu/gameboy/display/Display.java
@@ -1,9 +1,9 @@
 package com.emu.gameboy.display;
 
 public class Display {
-	Colour framebuffer[] = new Colour[160*144];
+	Colour framebuffer[] = new Colour[160*144]; //Creamos la pantalla de 160 * 144 pixeles
 	Colour palette[] = {
-			new Colour(255,255,255),
+			new Colour(255,255,255), //Paleta de escala de grises
 			new Colour(192,192,192),
 			new Colour(96,96,96),
 			new Colour(0,0,0)
